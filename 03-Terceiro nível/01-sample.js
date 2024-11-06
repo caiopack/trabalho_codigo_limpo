300dc95d612a9220d2deb491b071e9f2d5b07dbd
Caio <ccaiocsermak@gmail.com>
2024-11-06 10:51:20 -0300

    corrigida sample 01 do terceiro nível, foi incluído o principio de responsabilidade única e houveram correções de formatação do código, além do número mágico

--- 03-Terceiro nível/01-sample.js
+++ 03-Terceiro nível/01-sample.js
@@ -1,27 +1,41 @@
-async function buscarEDetalharNave() {
-    try {
-        const resposta = await fetch("https://swapi.dev/api/starships/9/");
-        const nave = await resposta.json();
+async function buscarNave() {
+    const respostaApi = await fetch("https://swapi.dev/api/starships/9/");
+    return await respostaApi.json();    
+}
 
-        const detalhesNave = `
+
+function detalhesDaNave(nave) {
+    return `
         Nome: ${nave.name}
         Modelo: ${nave.model}
         Fabricante: ${nave.manufacturer}
-        `;
+    `;
+}
 
-        console.log("Detalhes da Nave:");
-        console.log(detalhesNave);
 
-        const tamanhoTripulacao = parseInt(nave.crew);
-        if (tamanhoTripulacao > 100) {
-            console.log("Esta é uma nave grande.");
-        } else {
-            console.log("Esta é uma nave pequena.");
-        }
-
-    } catch (erro) {
-        console.error("Erro ao buscar detalhes da nave:", erro);
+function classificarTamanhoDaNave(nave) {
+    const tamanhoTripulacao = parseInt(nave.crew);
+    const tripulacaoNavePequena = 100;
+    if (tamanhoTripulacao > tripulacaoNavePequena) {
+        return "Esta é uma nave grande.";
+    } else {
+        return "Esta é uma nave pequena";
     }
 }
 
-buscarEDetalharNave();
+async function exibirResultadoDaBusca() {
+    try {
+        const pesquisarNave = await buscarNave(); 
+        const detalhesFornecidos = detalhesDaNave(pesquisarNave); 
+        const tamanhoDaNave = classificarTamanhoDaNave(pesquisarNave);  
+
+        console.log("Detalhes da Nave:");
+        console.log(detalhesFornecidos);
+        console.log(tamanhoDaNave);
+
+    } catch (erro) {
+        console.error("Erro ao buscar detalhes da nave:", erro);  
+    }
+}
+
+exibirResultadoDaBusca();  
