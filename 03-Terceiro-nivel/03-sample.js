8b00d1155704ddacb757254adb88211fee0bf844
Caio <ccaiocsermak@gmail.com>
2024-11-07 11:24:49 -0300

    Alderaan's not far away, corrigido 03 sample do terceiro nível

--- 03-Terceiro-nivel/03-sample.js
+++ 03-Terceiro-nivel/03-sample.js
@@ -1,27 +1,43 @@
-async function buscarEDetalharPlaneta(idPlaneta) {
+async function buscarPlaneta(idPlaneta) {
     try {
         const resposta = await fetch(`https://swapi.dev/api/planets/${idPlaneta}/`);
-        const planeta = await resposta.json();
-
-        const detalhesPlaneta = `
-        Nome: ${planeta.name}
-        Clima: ${planeta.climate}
-        População: ${planeta.population}
-        `;
-
-        console.log("Detalhes do Planeta:");
-        console.log(detalhesPlaneta);
-
-        const populacao = parseInt(planeta.population);
-        const populacaoMaximo = 1000000;
-        if (populacao > populacaoMaximo) {
-            console.log("Este planeta é muito populado.");
-        } else {
-            console.log("Este planeta tem uma população pequena.");
-        }
+        return await resposta.json();
     } catch (erro) {
         console.error("Erro ao buscar o planeta:", erro);
+        throw erro; 
     }
 }
 
-buscarEDetalharPlaneta(1);
+function exibirDetalhesPlaneta(planeta) {
+    const detalhesPlaneta = `
+    Nome: ${planeta.name}
+    Clima: ${planeta.climate}
+    População: ${planeta.population}
+    `;
+
+    console.log("Detalhes do Planeta:");
+    console.log(detalhesPlaneta);
+}
+
+function verificarPopulacaoPlaneta(planeta) {
+    const populacao = parseInt(planeta.population);
+    const populacaoMaximaPlanetaPequeno = 1000000;
+    if (populacao > populacaoMaximaPlanetaPequeno) {
+        console.log("Este planeta é muito populado.");
+    } else {
+        console.log("Este planeta tem uma população pequena.");
+    }
+}
+
+async function exibirResultadoDaBusca(idPlaneta) {
+    try {
+        const planeta = await buscarPlaneta(idPlaneta);
+        exibirDetalhesPlaneta(planeta);
+        verificarPopulacaoPlaneta(planeta);
+    } catch (erro) {
+        console.error("Erro ao buscar ou detalhar o planeta:", erro);
+    }
+}
+
+exibirResultadoDaBusca(1);
+
