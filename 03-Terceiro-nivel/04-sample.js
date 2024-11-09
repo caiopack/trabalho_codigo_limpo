5abcccd6297cb7db61b439cf87f9acab17753a9a
Caio <ccaiocsermak@gmail.com>
2024-11-09 18:49:34 -0300

    commit definitivo do terceiro nível, a sample 4 foi devidamente corrigida

--- 03-Terceiro-nivel/04-sample.js
+++ 03-Terceiro-nivel/04-sample.js
@@ -31,7 +31,7 @@ function verificarTamanhoDaNave(nave) {
     }
 }
 
-async function buscarPersonagemENave(idPersonagem) {
+async function exibirResultado(idPersonagem) {
     try {
         const personagem = await buscarPersonagem(idPersonagem);
 
@@ -48,55 +48,5 @@ async function buscarPersonagemENave(idPersonagem) {
     }
 }
 
-buscarPersonagemENave(1);
-async function buscarPersonagem(idPersonagem) {
-    try {
-        const respostaPersonagem = await fetch(`https://swapi.dev/api/people/${idPersonagem}/`);
-        const personagem = await respostaPersonagem.json();
-        return personagem;
-    } catch (erro) {
-        console.error("Erro ao buscar o personagem:", erro);
-        throw erro; 
-    }
-}
 
-async function buscarNave(urlNave) {
-    try {
-        const respostaNave = await fetch(urlNave);
-        const nave = await respostaNave.json();
-        return nave;
-    } catch (erro) {
-        console.error("Erro ao buscar a nave:", erro);
-        throw erro;
-    }
-}
-
-function verificarTamanhoDaNave(nave) {
-    const numeroLimite = 100;
-    const tripulacao = parseInt(nave.crew);
-
-    if (tripulacao > numeroLimite) {
-        return `A nave ${nave.name} é considerada grande com ${tripulacao} tripulantes.`;
-    } else {
-        return `A nave ${nave.name} é pequena com ${tripulacao} tripulantes.`;
-    }
-}
-
-async function exibirNave(idPersonagem) {
-    try {
-        const personagem = await buscarPersonagem(idPersonagem);
-
-        if (personagem.starships.length > 0) {
-            const nave = await buscarNave(personagem.starships[0]);
-            const resultado = verificarTamanhoDaNave(nave);
-            console.log(resultado);
-        } else {
-            console.log(`${personagem.name} não possui naves registradas.`);
-        }
-
-    } catch (erro) {
-        console.error("Erro ao buscar o personagem ou sua nave:", erro);
-    }
-}
-
-exibirNave(1);
+exibirResultado(1);
